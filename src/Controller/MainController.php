3b82a57c22bf311e46733abe0a70de834c6d7668
Amin Ossoul <aminoussoul@gmail.com>
2025-03-20 18:46:57 +0100

    debugg

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
 
   #[Route('/')]
   public function homepage(): Response
   {
-    return new Response('<strong>Starshop</strong>:your monopoly-blusting option for Starship parts!');
+
+    $startshipCount = 457;
+
+    $myShip = [
+      'name' => 'USS LaefyCruiser (NCC-0001)',
+      'class' => 'Garden',
+      'captain' => 'Levi Ackerman',
+      'status' => 'under Construction'
+    ];
+
+
+    return $this->render('main/homepage.html.twig', [
+      'numberOfStarships' => $startshipCount,
+      'myShip' => $myShip,
+    ]);
   }
 }
